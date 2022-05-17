1a20dcaaea2a4a64b11cd37e0b26d95795767ccf
123bhagesh <123bhagesh@gmail.com>
2022-05-17 22:57:42 +0530

    Assignment 1 complete

---
+++ ass1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Mob from './components/Mob'
+
+function App() {
+  return (
+    <div className="App">
+      <Mob></Mob>
+    </div>
+  );
+}
+
+export default App;
